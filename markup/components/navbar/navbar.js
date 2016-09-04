bdf456fc328d84d8e816c43e6b4c4d98488ca9e2
Антон Голомазов <agolomazov@s-get.ru>
2016-09-04 12:25:47 +0300

    Добавлен адаптив для навигации - (main page)

--- markup/components/navbar/navbar.js
+++ markup/components/navbar/navbar.js
@@ -1,1 +1,4 @@
 $('.top-menu__item:has(.dropdown-menu-wrapper)').addClass('dropdown-parent');
+$('.dropdown-parent').on('click', function (e) {
+    $('.dropdown-menu-wrapper', e.target).slideToggle(500);
+});
