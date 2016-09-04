413c54d5cc7f249746eea3a2adea2d6cc16f3212
Антон Голомазов <agolomazov@s-get.ru>
2016-09-04 20:30:37 +0300

    Блок со списком последних новостей - (main page)

--- markup/components/news-list/news-list.js
+++ markup/components/news-list/news-list.js
@@ -1,1 +1,24 @@
+$(document).ready(function () {
+
+    const windowWidth = $(window).outerWidth();
+
+    if (windowWidth > 800) {
+        $('.list-posts').equalize({
+            target: '.news-list__item'
+        });
+
+        $('.news-list__item-title').equalize();
+    }
+
+    $( window ).resize(function(){
+        if (windowWidth > 800) {
+            $('.list-posts').equalize({
+                target: '.news-list__item'
+            });
+
+            $('.news-list__item-title').equalize();
+        }
+    });
+
+});
 
