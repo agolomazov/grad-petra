ede19b93b8c64ddb9c8e26a78be11a1694d058e6
Антон Голомазов <agolomazov@s-get.ru>
2016-09-05 19:22:50 +0300

    Добавлен фильтр плагином mixitUp - (gallery page)

--- markup/components/gallery/gallery.js
+++ markup/components/gallery/gallery.js
@@ -1,1 +1,14 @@
-
+$(document).ready(function () {
+    $('#gallery__items-list').mixItUp({
+        selectors: {
+            target: '.gallery__item-image',
+            filter: '.filter'
+        },
+        animation: {
+            enable: true,
+            effects: 'fade scale',
+            duration: 600,
+            easing: 'ease',
+        },
+    });
+});
