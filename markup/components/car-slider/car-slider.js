27caa2399ddd151d54d5f9547ed50fcc8f7d4fce
Антон Голомазов <agolomazov@s-get.ru>
2016-09-06 18:10:13 +0300

    Добавлена карусель автомобилей для страницы АВТОПАРК

---
+++ markup/components/car-slider/car-slider.js
@@ -0,0 +1,27 @@
+$(document).ready(function () {
+
+    let owl = $('.car-carousel');
+    owl.owlCarousel({
+        items: 4,
+        navigation: false,
+        pagination: false,
+        rewindNav: true,
+        scrollPerPage: false,
+        autoPlay: 5000,
+        stopOnHover: true,
+        itemsCustom: [
+            [1280, 4],
+            [1024, 3],
+        ],
+    });
+
+
+    $('.car-carousel__control-item-prev').click(function () {
+        owl.trigger('owl.prev');
+    });
+
+    $('.car-carousel__control-item-next').click(function () {
+        owl.trigger('owl.next');
+    });
+
+});
