23117244cc95d4c595decd2e2239418110f05475
Majesko <majesko@gmail.com>
2016-09-08 14:03:56 +0300

    fix для отображения карт

--- markup/components/map/map.js
+++ markup/components/map/map.js
@@ -1,1 +1,26 @@
+$(document).ready(function () {
+    /*eslint-disable */
 
+    function init() {
+
+        let myMap;
+
+        myMap = new ymaps.Map("map", {
+            center: [59.934870, 30.330452],
+            zoom: 16,
+            controls: []
+        });
+
+        myMap.behaviors.disable('scrollZoom');
+
+        var myPlacemark = new ymaps.Placemark([59.936106, 30.322974] , {},
+            { iconLayout: 'default#image',
+                iconImageHref: '/static/img/content/baloon.png',
+                iconImageSize: [30, 48],
+                iconImageOffset: [-20, -47] });
+
+        myMap.geoObjects.add(myPlacemark);
+    }
+    ymaps.ready(init);
+    /*eslint-enable */
+});
