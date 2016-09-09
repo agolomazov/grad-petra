8b5fc27d56bce73a0492a70ffa21a41a8e10881d
Majesko <majesko@gmail.com>
2016-09-09 21:00:51 +0300

    еще фикс для отзывов

--- markup/components/carousel/carousel.js
+++ markup/components/carousel/carousel.js
@@ -1,11 +1,11 @@
 $(document).ready(function () {
     $('#owl-demo').owlCarousel({
-        navigation: true,
         slideSpeed: 300,
         paginationSpeed: 400,
         singleItem: true,
         // autoPlay: true,
-        autoWidth: true
+        autoWidth: true,
+        navigation: false
         // "singleItem:true" is a shortcut for:
         // items : 1,
         // itemsDesktop : false,
